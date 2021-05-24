ebd743104cc31b00883cda83810fabf7e9b10974
YuBeomGon <cleo8131@naver.com>
2021-05-25 07:38:34 +0900

    evaluation code

--- train_paps.py
+++ train_paps.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageEnhance
 import albumentations as A
 import albumentations.pytorch
-from tqdm.notebook import tqdm
+# from tqdm.notebook import tqdm
+from tqdm import tqdm
 
 import cv2
 import re
@@ -16,10 +17,13 @@ import time
 
 import sys
 sys.path.append('../')
-from retinanet import coco_eval
-from retinanet import csv_eval
 from retinanet import model
+from retinanet import coco_eval
+from retinanet import paps_eval
+from retinanet import csv_eval
+
 # from retinanet import retina
+# from retinanet.paps_eval import evaluate_paps
 from retinanet.dataloader import *
 from retinanet.anchors import Anchors
 from retinanet.losses import *
@@ -48,11 +52,11 @@ def main(args=None):
     parser = argparse.ArgumentParser(description='Simple paps training script for training a RetinaNet network.')
     parser.add_argument('--depth', help='Resnet depth, must be one of 18, 34, 50, 101, 152', type=int, default=50)
     parser.add_argument('--epochs', help='Number of epochs', type=int, default=160)
-    parser.add_argument('--batch_size', help='Number of batchs', type=int, default=128)    
+    parser.add_argument('--batch_size', help='Number of batchs', type=int, default=64)    
     parser.add_argument('--train_data', help='train data file', default='data/train.npy')    
     parser.add_argument('--test_data', help='test data file', default='data/test.npy')   
-    parser.add_argument('--saved_dir', help='saved dir', default='trained_models/resnet50_320_ex_bn/') 
-    parser.add_argument('--gpu_num', help='default gpu', type=int, default=4) 
+    parser.add_argument('--saved_dir', help='saved dir', default='trained_models/resnet50_320/') 
+    parser.add_argument('--gpu_num', help='default gpu', type=int, default=5) 
     parser.add_argument('--freeze_ex_bn', help='default gpu', type=bool, default=False) 
     
     parser = parser.parse_args(args)
@@ -65,42 +69,40 @@ def main(args=None):
     torch.cuda.set_device(device) # change allocation of current GPU
     print(device)
     print ('Current cuda device ', torch.cuda.current_device()) # check
-    device_ids = [4,3,2]
+    device_ids = [3,2,1]
     
     PATH_TO_WEIGHTS = 'coco_resnet_50_map_0_335_state_dict.pt'
-    pretrained_retinanet = model.resnet50(num_classes=80, device=device)
-    pretrained_retinanet.load_state_dict(torch.load(PATH_TO_WEIGHTS,map_location=device), strict=False)
+    pre_retinanet = model.resnet50(num_classes=80, device=device)
+    pre_retinanet.load_state_dict(torch.load(PATH_TO_WEIGHTS, map_location=device), strict=False)
+    pre_retinanet.classificationModel.output = nn.Conv2d(256, 9*2, kernel_size=3, padding=1)
 
-    retinanet = model.resnet50(num_classes=2, device=device)
-    for param, state in zip(pretrained_retinanet.parameters(), pretrained_retinanet.state_dict()) :
-        #print(state)
-        if 'classificationModel' not in state :
-            retinanet.state_dict()[state] = param
-
-    for param, state in zip(pretrained_retinanet.fpn.parameters(), pretrained_retinanet.fpn.state_dict()) :
-        #print(state)
-        retinanet.fpn.state_dict()[state] = param
-
-    for param, state in zip(pretrained_retinanet.regressionModel.parameters(), pretrained_retinanet.regressionModel.state_dict()) :
-        #print(state)
-        retinanet.regressionModel.state_dict()[state] = param   
+    ret_model = model.resnet50(num_classes=2, device=device)
+    ret_model.load_state_dict(pre_retinanet.state_dict())
+    del pre_retinanet 
+    
+    state_dict = ret_model.state_dict()
+    for s in state_dict:
+        if 'bn' in s :
+            if 'weight' in s :
+                shape = state_dict[s].shape
+                state_dict[s] = torch.zeros(shape)
+            elif 'bias' in s :
+                shape = state_dict[s].shape
+                state_dict[s] = torch.ones(shape)            
+    
+    ret_model.load_state_dict(state_dict)
+    
+#     if parser.freeze_ex_bn :
+#         for k, p in zip(ret_model.module.state_dict(), ret_model.module.parameters()) :
+#             if 'bn' not in k :
+#                 p.requires_grad = False  
         
-    for param, state in zip(pretrained_retinanet.classificationModel.parameters(), pretrained_retinanet.classificationModel.state_dict()) :
-        #print(state)
-        retinanet.classificationModel.state_dict()[state] = param      
-        
-    if parser.freeze_ex_bn :
-        for k, p in zip(retinanet.module.state_dict(), retinanet.module.parameters()) :
-            if 'bn' not in k :
-                p.requires_grad = False  
-                
-    del pretrained_retinanet
-        
-    retinanet = torch.nn.DataParallel(retinanet, device_ids = [4,3,2], output_device=GPU_NUM).to(device)
-    # retinanet = DataParallelModel(retinanet, device_ids = device_ids)
-    retinanet.to(device)
-    # retinanet.cuda()
-#     retinanet.module.freeze_bn()     
+    ret_model = torch.nn.DataParallel(ret_model, device_ids = [3,2,1], output_device=GPU_NUM).to(device)
+    ret_model.module.freeze_ex_bn(False)
+    # ret_model = DataParallelModel(ret_model, device_ids = device_ids)
+    ret_model.to(device)
+    # ret_model.cuda()
+#     ret_model.module.freeze_bn()     
     
     batch_size = parser.batch_size
     dataset_train = PapsDataset('data/', set_name='train_2class',
@@ -110,72 +112,10 @@ def main(args=None):
         dataset_train,
         batch_size=batch_size,
         shuffle=True,
-        num_workers=32,
+        num_workers=16,
         pin_memory=True,
         collate_fn=collate_fn
     )
-
-    criterion = FocalLoss(device)
-    criterion = criterion.to(device)
-    retinanet.training = True
-    
-    optimizer = optim.Adam(retinanet.parameters(), lr = 1e-7)
-    scheduler = CosineAnnealingWarmUpRestarts(optimizer, T_0=30, T_mult=2, eta_max=0.0004,  T_up=10, gamma=0.5)    
-    
-    loss_per_epoch = 0.6
-    for epoch in range(parser.epochs) :
-        total_loss = 0
-        tk0 = tqdm(train_data_loader, total=len(train_data_loader), leave=False)
-        EPOCH_LEARING_RATE = optimizer.param_groups[0]["lr"]
-        start_time = time.time()
-        print("*****{}th epoch, learning rate {}".format(epoch, EPOCH_LEARING_RATE))
-
-        for step, data in enumerate(tk0) :
-            images, box, label, targets = data
-            batch_size = len(images)
-
-        #     images = list(image.to(device) for image in images)
-            c, h, w = images[0].shape
-            images = torch.cat(images).view(-1, c, h, w).to(device)
-    #         print(images.shape)
-    #         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-            targets = [ t.to(device) for t in targets]
-
-    #         classification_loss, regression_loss = retinanet([images, targets])
-            outputs = retinanet([images, targets])
-            classification, regression, anchors, annotations = (outputs)
-            classification_loss, regression_loss = criterion(classification, regression, anchors, annotations)
-
-    #         output = retinanet(images)
-    #         features, regression, classification = output
-    #         classification_loss, regression_loss = criterion(classification, regression, modified_anchors, targets)    
-            classification_loss = classification_loss.mean()
-            regression_loss = regression_loss.mean()
-            loss = classification_loss + regression_loss 
-            total_loss += loss.item()
-
-            if step % 100 == 0:
-                print('lr {} batch_loss {} cls {} reg {} avg {}'.format(optimizer.param_groups[0]["lr"], loss.item(), classification_loss.item(), 
-                                regression_loss.item(), total_loss/(step+1)
-                ))
-#                 tk0.set_postfix(lr=optimizer.param_groups[0]["lr"], batch_loss=loss.item(), cls_loss=classification_loss.item(), 
-#                                 reg_loss=regression_loss.item(), avg_loss=total_loss/(step+1))
-
-            optimizer.zero_grad()
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(retinanet.parameters(), 0.02)
-            optimizer.step()   
-
-        print('{}th epochs loss is {}'.format(epoch, total_loss/(step+1)))
-        if loss_per_epoch > total_loss/(step+1):
-            print('best model is saved')
-            torch.save(retinanet.state_dict(), parser.saved_dir + 'best_model.pt')
-            loss_per_epoch = total_loss/(step+1)
-
-        scheduler.step()
-        print('epoch training time is ', time.time() - start_time)
-
-    torch.save(retinanet.state_dict(), parser.saved_dir + 'model.pt')
     
     dataset_val = PapsDataset('data/', set_name='val_2class',
                                 transform=val_transforms)
@@ -186,138 +126,94 @@ def main(args=None):
         shuffle=False,
         num_workers=2,
         collate_fn=collate_fn
-    )    
+    )        
+
+    criterion = FocalLoss(device)
+    criterion = criterion.to(device)
+    ret_model.training = True
     
-    retinanet.eval()
-    start_time = time.time()
-    threshold = 0.1
-    results = []
-    GT_results = []
-    image_ids = []
-    cnt = 0
-    scores_list = []
+    optimizer = optim.Adam(ret_model.parameters(), lr = 1e-7)
+    scheduler = CosineAnnealingWarmUpRestarts(optimizer, T_0=20, T_mult=2, eta_max=0.0008,  T_up=5, gamma=0.5)    
+    
+    loss_per_epoch = 0.6
+    optimizer.param_groups[0]["lr"] = 0.00002
 
-    for index, data in enumerate(tqdm(val_data_loader)) :
-    #     if cnt > 500 :
-    #         break
-    #     cnt += 1
-        with torch.no_grad():        
-            images, tbox, tlabel, targets = data
+#     paps_eval.evaluate_paps(dataset=dataset_val, 
+#                   dataloader=val_data_loader, 
+#                   model=ret_model, 
+#                   saved_dir=parser.saved_dir, 
+#                   device = device,
+#                   threshold=0.5) 
+    
+    for epoch in range(parser.epochs) :
+        if epoch == int(parser.epochs*0.2) :
+            ret_model.module.freeze_ex_bn(True)
+        total_loss = 0
+        tk0 = tqdm(train_data_loader, total=len(train_data_loader), leave=False)
+        EPOCH_LEARING_RATE = optimizer.param_groups[0]["lr"]
+        start_time = time.time()
+        print("*****{}th epoch, learning rate {}".format(epoch, EPOCH_LEARING_RATE))
+
+        for step, data in enumerate(tk0) :
+            if step > len(train_data_loader)/4 and epoch < int(parser.epochs*0.8) :
+                break            
+            images, box, label, targets = data
             batch_size = len(images)
-    #         print(tbox)
-    #         print(len(tbox[0]))
 
+        #     images = list(image.to(device) for image in images)
             c, h, w = images[0].shape
             images = torch.cat(images).view(-1, c, h, w).to(device)
+    #         print(images.shape)
+    #         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            targets = [ t.to(device) for t in targets]
 
-            outputs = retinanet(images)
-            scores, labels, boxes = (outputs)
+    #         classification_loss, regression_loss = ret_model([images, targets])
+            outputs = ret_model([images, targets])
+            classification, regression, anchors, annotations = (outputs)
+            classification_loss, regression_loss = criterion(classification, regression, anchors, annotations)
 
-            scores = scores.cpu()
-            labels = labels.cpu()
-            boxes  = boxes.cpu()  
+    #         output = ret_model(images)
+    #         features, regression, classification = output
+    #         classification_loss, regression_loss = criterion(classification, regression, modified_anchors, targets)    
+            classification_loss = classification_loss.mean()
+            regression_loss = regression_loss.mean()
+            loss = classification_loss + regression_loss 
+            total_loss += loss.item()
 
-            scores_list.append(scores)
+#             if step % 100 == 0:
+#                 print('lr {} batch_loss {} cls {} reg {} avg {}'.format(optimizer.param_groups[0]["lr"], loss.item(), classification_loss.item(), 
+#                                 regression_loss.item(), total_loss/(step+1)
+#                 ))
 
-            if boxes.shape[0] > 0:
-                # change to (x, y, w, h) (MS COCO standard)
-                boxes[:, 2] -= boxes[:, 0]
-                boxes[:, 3] -= boxes[:, 1]
-    #             print(boxes)
+            optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(ret_model.parameters(), 0.02)
+            optimizer.step()   
 
-                # compute predicted labels and scores
-                #for box, score, label in zip(boxes[0], scores[0], labels[0]):
-                for box_id in range(boxes.shape[0]):
-                    score = float(scores[box_id])
-                    label = int(labels[box_id])
-                    box = boxes[box_id, :]
+        print('{}th epochs loss is {}'.format(epoch, total_loss/(step+1)))
+        if loss_per_epoch > total_loss/(step+1):
+            print('best model is saved')
+            torch.save(ret_model.state_dict(), parser.saved_dir + 'best_model.pt')
+            loss_per_epoch = total_loss/(step+1)
 
-                    # scores are sorted, so we can break
-                    if score < threshold:
-                        break
+        scheduler.step()
+        print('epoch training time is ', time.time() - start_time)
+#         if epoch % 20 == 0 :
+#             paps_eval.evaluate_paps(dataset=dataset_val, 
+#               dataloader=val_data_loader, 
+#               model=ret_model, 
+#               saved_dir=parser.saved_dir, 
+#               device = device,
+#               threshold=0.5)    
+#         ret_model.train()
 
-                    # append detection for each positively labeled class
-                    image_result = {
-                        'image_id'    : dataset_val.image_ids[index],
-                        'category_id' : dataset_val.label_to_coco_label(label),
-                        'score'       : float(score),
-                        'bbox'        : box.tolist(),
-                    }
-
-                    # append detection to results
-                    results.append(image_result)
-
-            if len(tbox[0]) > 0:    
-
-                # compute predicted labels and scores
-                #for box, score, label in zip(boxes[0], scores[0], labels[0]):
-                for box_id in range(len(tbox[0])):
-                    score = float(0.99)
-                    label = (tlabel[0][box_id])
-                    box = list(tbox[0][box_id])
-                    box[2] -= box[0]
-                    box[3] -= box[1]             
-
-                    # append detection for each positively labeled class
-                    image_result = {
-                        'image_id'    : dataset_val.image_ids[index],
-                        'category_id' : dataset_val.label_to_coco_label(label),
-                        'score'       : float(score),
-                        'bbox'        : list(box),
-                    }
-
-                    # append detection to results
-                    GT_results.append(image_result)                
-
-            # append image to list of processed images
-            image_ids.append(dataset_val.image_ids[index])
-
-            # print progress
-            print('{}/{}'.format(index, len(dataset_val)), end='\r')    
-
-    if not len(results):
-        print('No object detected')
-    print('GT_results', len(GT_results))    
-    print('pred_results', len(results))    
-
-    # write output
-    json.dump(results, open(parser.saved_dir + '{}_bbox_results.json'.format(dataset_val.set_name), 'w'), indent=4)
-    # write GT
-    json.dump(GT_results, open(parser.saved_dir + '{}_GTbbox_results.json'.format(dataset_val.set_name), 'w'), indent=4)     
-
-    print('validation time :', time.time() - start_time)
-
-
-    # load results in COCO evaluation tool
-    coco_true = dataset_val.coco
-    coco_pred = coco_true.loadRes(parser.saved_dir + '{}_bbox_results.json'.format(dataset_val.set_name))
-    coco_gt = coco_true.loadRes(parser.saved_dir + '{}_GTbbox_results.json'.format(dataset_val.set_name))
-
-    # run COCO evaluation
-    # coco_eval = COCOeval(coco_true, coco_pred, 'bbox')
-    print("******************total*********************")
-    coco_eval = COCOeval(coco_gt, coco_pred, 'bbox')
-    coco_eval.params.imgIds = image_ids
-    # coco_eval.params.catIds = [0]
-    coco_eval.evaluate()
-    coco_eval.accumulate()
-    coco_eval.summarize()    
-    
-    print("******************Abnormal*********************")
-    coco_eval = COCOeval(coco_gt, coco_pred, 'bbox')
-    coco_eval.params.imgIds = image_ids
-    coco_eval.params.catIds = [0]
-    coco_eval.evaluate()
-    coco_eval.accumulate()
-    coco_eval.summarize()   
-    
-    print("******************Normal*********************")
-    coco_eval = COCOeval(coco_gt, coco_pred, 'bbox')
-    coco_eval.params.imgIds = image_ids
-    coco_eval.params.catIds = [1]
-    coco_eval.evaluate()
-    coco_eval.accumulate()
-    coco_eval.summarize()       
+    torch.save(ret_model.state_dict(), parser.saved_dir + 'model.pt')
+    paps_eval.evaluate_paps(dataset=dataset_val, 
+      dataloader=val_data_loader, 
+      model=ret_model, 
+      saved_dir=parser.saved_dir, 
+      device = device,
+      threshold=0.5)  
         
 if __name__ == '__main__':
     main()        
